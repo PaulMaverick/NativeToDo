f96d4081429d04a39272ee0dbb37b0a3459916bb
Paul <paulpastor122297@gmail.com>
2025-01-22 07:57:15 +0800

    initial toolbar.tsx

--- component/ToolBar.tsx
+++ component/ToolBar.tsx
@@ -1,16 +1,29 @@
+import {  AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
 import {View, Text, StyleSheet} from 'react-native';
 
 export default function ToolBarView() {
     return (
         <View style={styles.toolBarContainer}>
-            <Text style={styles.toolBarText}>Toolbar</Text>
+           <View>
+            {/*updates */}
+             
+             <MaterialCommunityIcons name="update" size={20} color="white" />
+           </View>
+           <View>
+                <AntDesign name="filter" size={20} color="white" />
+           </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     toolBarContainer: {
-
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 15,
+        borderBottomColor: '#4f4d4d',
+        borderBottomWidth: 2,
     },
     toolBarText: {
         color: 'white'
